b18982a0e0461e06e021aad4897ef07d1a183f2e
DanielSistemasExpertos <daniel.angel@sistemasexpertos.cl>
2020-01-31 12:30:07 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://dangel:G9OzeLqY5ZG1CVz2@cluster0-ceitq.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
